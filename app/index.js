55b891f81261de414bf8d5f7d4de458dd2790755
Mark <markdrew53@gmail.com>
2018-02-28 02:08:44 -0600

    eslint added

---
+++ app/index.js
@@ -0,0 +1,24 @@
+// Packages
+const {app, BrowserWindow} = require('electron');
+const logger = require('electron-timber');
+
+const browserOptions = {
+  titleBarStyle: 'hiddenInset',
+  backgroundColor: '#000000',
+  minWidth: 505,
+  minHeight: 232
+};
+
+let win = null;
+
+try {
+  require('electron-reloader')(module);
+} catch (err) {
+  console.log(err);
+}
+
+app.on('ready', () => {
+  win = new BrowserWindow(browserOptions);
+  logger.log('Running main processes');
+  win.loadURL(`file://${__dirname}/index.html`);
+});
